030fdd09ad06d4be103207667ffdef31bac1356b
Samir Cabrera <samircm1406@gmail.com>
2020-12-05 02:10:00 +0100

    arreglando dialog

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { CategoryModalComponent } from './pages/homepage/modals/category-modal/category-modal.component';
+import { ListSiteModalComponent } from './pages/homepage/modals/list-site-modal/list-site-modal.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -9,7 +11,7 @@ describe('AppComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent, ListSiteModalComponent, CategoryModalComponent
       ],
     }).compileComponents();
   }));
